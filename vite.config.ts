ec28affe75b7c9c3ce27f5645449e5b9bc17638c
ashish <ashishd@backbase.com>
2025-08-14 16:38:17 +0530

    Initial portfolio commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// Change /ashish-portfolio/ to match your repo name
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [react()],
+  base: '/ashish-portfolio/',
 });
