4eb0af77bbe2528a672a9bbbfb9c981ba9ede5f5
ashish <ashishd@backbase.com>
2025-08-14 16:53:46 +0530

    deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// Change /ashish-portfolio/ to match your repo name
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
-  plugins: [react()],
-  base: '/ashish-portfolio/',
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
